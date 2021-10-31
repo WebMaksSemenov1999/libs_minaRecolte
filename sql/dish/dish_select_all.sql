5ea4eaaafe3cb64ab32882a45315ea4e1dc572d2
Semenov Maksim <Pavel@mail.ru>
2021-10-31 20:04:01 +0400

    sql/dish/dish_select_all.sql

---
+++ sql/dish/dish_select_all.sql
@@ -0,0 +1,4 @@
+SELECT * FROM "dish"  
+LEFT JOIN "categoires_dish" ON "dish"."id_categories" = "categoires_dish"."id"
+LEFT JOIN "dish_tag" ON "dish_tag"."id_dish" = "dish"."id"
+LEFT JOIN "tag" ON "dish_tag"."id_tag" = "tag"."id"
