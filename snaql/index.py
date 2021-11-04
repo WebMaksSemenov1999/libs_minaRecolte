088e3c37972f9dd8edc1cb96f5a1f8b1cf0298ff
Semenov Maksim <Pavel@mail.ru>
2021-11-04 14:00:48 +0400

    delete __pycache__ and .gitignore __pycache__

--- snaql/index.py
+++ snaql/index.py
@@ -1,1 +1,15 @@
 from bd import conn
+from sql_fun.users import users_inset_admin
+
+your_sql = users_inset_admin(
+    fio='Maks Semen', 
+    nik='dadas4345asdtzz',
+    email='das1321asdzzz@mail.ru',
+    password='pdsf932gfz',
+    avatar='https://placepic.ru/wp-content/uploads/2018/01/art-krasivyie-kartinki-Putin-politika-1331294.jpeg',
+    is_active='1',
+    is_admin='1',
+    is_user='1'
+    )
+cur = conn.cursor()
+cur.execute(your_sql)
--- snaql/sql_fun/users.py
+++ snaql/sql_fun/users.py
@@ -1,5 +1,5 @@
 from sql_fun.index import snaql_factory
-# users_inset_admin = snaql_factory.load_queries('users/users_inset_admin.sql')
+users_inset_admin = snaql_factory.load_queries('users/users_inset_admin.sql').users_inset_user_admin
 users_inset_user = snaql_factory.load_queries('users/users_inset_user.sql').users_inset_user
 # users_select_all = snaql_factory.load_queries('users/users_select_all.sql')
 # users_update_active_true = snaql_factory.load_queries('users/users_update_active-true')
