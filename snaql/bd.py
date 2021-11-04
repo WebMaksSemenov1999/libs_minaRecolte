86b8ac1d20fe9fe005b32fbb587a9d3afe58928d
Semenov Maksim <Pavel@mail.ru>
2021-11-04 11:31:57 +0400

    min-29 and min-30

---
+++ snaql/bd.py
@@ -0,0 +1,6 @@
+import psycopg2
+conn = psycopg2.connect(
+    dbname="MinaRecolte", 
+    user="root", 
+    password="JDAKF032498FFjfs3",
+)
---
+++ snaql/install.bat
@@ -0,0 +1,2 @@
+pip install snaql
+pip install psycopg2-binary
