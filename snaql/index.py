2993b696884b3c25fb3d6df0ecdf1478e5d59296
Semenov Maksim <Pavel@mail.ru>
2021-11-04 19:33:57 +0400

    visible python

--- snaql/index.py
+++ snaql/index.py
@@ -1,32 +1,28 @@
+import os
+import sys
+
+main = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(main)
+
 from bd import conn
 from sql_fun.users import users_select_all
+from libs_back.parsring_order import ParsringOrder
+
+
+
 params = {
     'limit': '2',
-    'offeset':'2',
-    'order':{
+    'offeset': '2',
+    'order': {
         'fio': True,
         'email': False,
         'nik': False,
         'is_active': False,
         'is_admin': False,
-        'is_user':False,
+        'is_user': False,
     }
-} 
- 
-def ParsringOrder(obj):
-    stringOrder = ""
-    for key in obj:
-        if obj[key] == True:
-            stringOrder += key + ' ' +  'DESC,'
-        if obj[key] == False:
-            stringOrder += key + ' ' +  'ASC,'
-    
-    return stringOrder[0:-1]
-
+}
 params['order'] = ParsringOrder(params['order'])
- 
-
-
 your_sql = users_select_all(**params)
 cur = conn.cursor()
 print(your_sql)
