9414c08ffbe8c3021f57150a96362e7289200b93
Maks <semenov@mail.ru>
2021-11-07 16:59:27 +0300

    создание пользователя изменение логики + консоль количество созданных пользователей

--- python/create_users/create_users.py
+++ python/create_users/create_users.py
@@ -14,7 +14,6 @@ users = []
 niks = []
 emails = []
 
-
 def saveUsers(user):
     cursor = conn.cursor()
     cursor.execute(f'INSERT INTO "users" ("fio", "nik", "email", "password", "avatar", "is_active", "is_admin", '
@@ -70,6 +69,7 @@ def getImage():
 def generatorUser(count):
     count_is_adm = 0
     count_is_act = 0
+    indexUsers = -1
 
     for i in range(count):
         response = requests.get(
@@ -99,8 +99,11 @@ def generatorUser(count):
                 'is_active': is_active
             }
         )
+        print("Пользователь создан " + str(indexUsers))
+        indexUsers = indexUsers + 1
+        saveUsers(users[indexUsers])
 
-    for user in users:
-        saveUsers(user)
+    # for user in users:
+        # saveUsers(user)
 
-generatorUser(2)
+generatorUser(150)
--- python/create_users/install.bat
+++ python/create_users/install.bat
@@ -1,3 +1,4 @@
 pip install psycopg2-binary
 pip install bs4
 pip install bcrypt
+pip install requests
